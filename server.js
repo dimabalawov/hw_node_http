e7d503b18d153a64fca909f387e24edc35436a3b
dimabalawov <dimabalawov@gmail.com>
2024-09-09 14:22:19 +0300

    POST

--- server.js
+++ server.js
@@ -2,25 +2,56 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
-http.createServer(function(req, res){
+
+http.createServer(function(req, res) {
     var path = url.parse(req.url).pathname;
 
-    path = 'index.html';
-    
-fs.readFile(path, function(err, data){
-    if(err){
-        console.log(err);
-        res.writeHead(404, { 'Content-Type' : 'text/plain'});
+    if (req.method === 'GET' && path === '/') {
+        fs.readFile('index.html', function(err, data) {
+            if (err) {
+                console.log(err);
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+                res.end('Not Found!');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write(data.toString());
+                console.log('data was sent');
+                res.end();
+            }
+        });
+    } 
+    else if (req.method === 'POST' && path === '/login') {
+        let body = '';
+        req.on('data', function(chunk) {
+            body += chunk;
+        });
+
+        req.on('end', function() {
+            var postData = new URLSearchParams(body);
+
+            var login = postData.get('login');
+            var password = postData.get('password');
+
+            console.log('Логин: ' + login);
+            console.log('Пароль: ' + password);
+            var logData = `Логин: ${login}, Пароль: ${password}\n`;
+
+            fs.appendFile('logins.txt', logData, function(err) {
+                if (err) {
+                    console.log('Ошибка при записи в файл:', err);
+                } else {
+                    console.log('Данные успешно записаны в файл.');
+                }
+            });
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Login and password received');
+        });
+    } 
+    else {
+
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('Not Found!');
     }
-    else{
-        res.writeHead(200, { 'Content-Type' : 'text/html'});
-        res.write(data.toString());
-        console.log('data was sent');
-        res.end();
-    }
-});
-
-}).listen(8080, function(){
+}).listen(8080, function() {
     console.log('Server starting!');
 });
